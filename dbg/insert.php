73bec8cc6ee1797f3b3c7b2d6c7f84ca12fcc401
Your Name <sobolentsev@gmail.com>
2016-02-13 21:50:23 +0300

    Add HTML pages for debugging. Test PHP CRow set/getKeyColumnName

---
+++ dbg/insert.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<html>
+<body>
+
+<?php
+echo "Test for PHP-DAO insert method";
+$db = new Counter;
+$db->open();
+$row = new CRow;
+echo "<br> PASSED: new CRow";
+$row->setKeyColumnName("id");
+$keyName = $row->getKeyColumnName();
+echo "<br> key column name:";
+print $keyName;
+$row->addColumn("name", "test");
+echo "<br> PASSED: row->addColumn";
+$id = $db->insert("person", $row);
+echo "<br> PASSED: row->insert";
+echo "<br> LAST Inserted key:";
+print $id;
+$rows = new CRows;
+echo "<br> PASSED: CRows";
+echo "<br> size:";
+print $rows->size();
+echo "<br> PASSED: rows->size()";
+
+$rows->pushBack($row);
+echo "<br> PASSED: rows->pushBack()";
+echo "<br> size:";
+print $rows->size();
+
+echo "<br> Read table data ...";
+
+$rowsRes = new CRows;
+$db->read("person", $rowsRes);
+$it = $rowsRes->createIterator();
+for($it->first(); !$it->isDone(); $it->next()) {
+    $row_it = $it->currentItem();
+    echo "<br> ";
+    print $row_it->getColumnValue("id");
+    echo " |";
+    print $row_it->getColumnValue("name");
+}
+
+echo "<br>=== TEST PASSED ===";
+
+?>
+
+</body>
+</html>
